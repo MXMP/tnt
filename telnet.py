00dcd7be05b79e3895492b26fd71e578eefcfa1a
mxmp <bandoftoys@gmail.com>
2018-01-14 23:41:07 +0300

    Добавлен файл с конфигом -> убран отдельный файл для авторизации.

--- telnet.py
+++ telnet.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python3
 
-from getpass import getpass
 from time import sleep
 import socket
 import os
+import sys
+import configparser
 from telnetlib import Telnet
 from multiprocessing.dummy import Pool as ThreadPool
 
 
 def send_commands(host):
-    """ Подключается к хосту, авторизуется, отсылает команды из файла, сохраняет конфигурацию, пишет вывод в 
-    файл отчета."""
-    global user, password, commands
+    """ Подключается к хосту, авторизуется, отсылает команды из файла, сохраняет конфигурацию (если нужно),
+    пишет вывод в файл отчета."""
+    global user, password, commands, config
+    reports_foldername = config['reports']['foldername']
+
     print('Connect to host: {}'.format(host))
     try:
-        conn = Telnet(host, 23, 10)
+        conn = Telnet(host, config['telnet'].getint('Port'), config['telnet'].getint('Timeout'))
     except (socket.timeout, OSError):
         # если при подключении возник таймаут, значит скорее всего устройство недоступно
         print('Can\'t connect to host: {}'.format(host))
         # создаем файл по шаблону "fail-<host>.txt" и пишем в него, что хост в дауне
-        write_to_file(os.path.join('reports', 'fail-' + host + '.txt'), ['Error\n', 'Host is down\n'])
+        write_to_file(os.path.join(reports_foldername, 'fail-' + host + '.txt'),
+                      ['Error\n', 'Host is down\n'])
     else:
         # если подключение прошло успешно, то передаем логин/пароль пользователя
         conn.write(user.encode('ascii') + b'\n')
         conn.write(password.encode('ascii') + b'\n')
+
         # шлем команды
         for command in commands:
             print('Send "{}" to {}'.format(command, host))
             conn.write(command.encode('ascii') + b'\n')
-        # сохранием конфигурацию устройства
-        conn.write(b'save\n')
-        # здесь выжидаем 5 секунд, т.к. сохранение конфига происходит не сразу
-        sleep(5)
-        # пишем в файл вывод
+
+        # если нужно сохранием конфигурацию устройства
+        if config['hosts'].getboolean('do_save'):
+            conn.write(b'save\n')
+            # здесь выжидаем 5 секунд, т.к. сохранение конфига происходит не сразу
+            sleep(5)
+
+        # разлогиниваемся
         conn.write(b'logout\n')
+
+        # пишем в файл вывод
         try:
-            write_to_file(os.path.join('reports', host + '.txt'), conn.read_all().decode('ascii'))
+            write_to_file(os.path.join(reports_foldername, host + '.txt'), conn.read_all().decode('ascii'))
         except socket.timeout:
-            write_to_file(os.path.join('reports', 'output_fail-' + host + '.txt'), ['Error\n', 'Host is down\n'])
+            write_to_file(os.path.join(reports_foldername, 'output_fail-' + host + '.txt'),
+                          ['Error\n', 'Host is down\n'])
         finally:
             conn.close()
 
@@ -61,28 +72,26 @@ def write_to_file(filename, data):
 
 
 if __name__ == '__main__':
-    # путь до файла с логином и паролем
-    auth_file = 'auth.txt'
-    # если файл с данными для авторизации существует, то берем логин и пароль из него
-    if os.path.isfile(auth_file):
-        auth = get_from_file(auth_file)
-        user = auth[0]
-        password = auth[1]
-    else:
-        # спрашиваем у пользователя его логин
-        user = input('Enter your remote account: ')
-        # спрашиваем у пользователя его пароль
-        password = getpass('Password: ')
+    # работаем только если есть файл конфига
+    if os.path.isfile('config.ini'):
+        # читаем конфиг
+        config = configparser.ConfigParser()
+        config.read('config.ini')
 
-    if password:
+        # данные для авторизации
+        user = config['auth']['User']
+        password = config['auth']['Password']
+
         # читаем из файлов хосты, к которым нужно коннектится, и комманды, которые нужно передавать
-        commands = get_from_file('commands.txt')
-        hosts = get_from_file('hosts.txt')
-        # если папка с отчетами уже создана просим удалить или переименовать ее
+        commands = get_from_file(config['commands']['filename'])
+        hosts = get_from_file(config['hosts']['filename'])
+
         try:
-            os.mkdir('reports')
+            os.mkdir(config['reports']['foldername'])
         except FileExistsError:
+            # если папка с отчетами уже создана просим удалить или переименовать ее
             print('Folder for reports already exists. Please delete or rename it.')
+            sys.exit()
         else:
             # создаем пул с 4 воркерами
             pool = ThreadPool(4)
@@ -90,3 +99,6 @@ if __name__ == '__main__':
             pool.map(send_commands, hosts)
             pool.close()
             pool.join()
+    else:
+        print('Please create "config.ini" file.')
+        sys.exit()
